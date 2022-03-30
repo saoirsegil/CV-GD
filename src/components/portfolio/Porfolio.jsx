197b8a26378eef4617ba0486e1687e55422f39d1
Jayson Alihassan Diaz <jaysonprincediaz@gmail.com>
2022-03-30 19:50:48 +0800

    Portfolio and Testimonials

--- src/components/portfolio/Porfolio.jsx
+++ src/components/portfolio/Porfolio.jsx
@@ -1,8 +1,107 @@
 import React from 'react';
 import './Portfolio.css';
 
+import Birthday from '../../assets/Ultimate-Celebration.jpg';
+import Gym from '../../assets/Strive-Poster.jpg';
+import Menu from '../../assets/Small-Kitchen-Menu.jpg';
+import Jelly from '../../assets/Small-Kitchen.jpg';
+import Logo from '../../assets/Wellness-Coach.jpg';
+import Trifold from '../../assets/Tri-fold.jpg';
+
 const Porfolio = () => {
-  return <div>Porfolio</div>;
+  return (
+    <section id="Portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Birthday} alt="" />
+          </div>
+          <h3>Birthday Poster</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Birthday} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Gym} alt="" />
+          </div>
+          <h3>Service Poster</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Gym} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Jelly} alt="" />
+          </div>
+          <h3>Product Poster</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Jelly} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Menu} alt="" />
+          </div>
+          <h3>Menu Board</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Menu} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Logo} alt="" />
+          </div>
+          <h3>Wellness Logo</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Logo} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Trifold} alt="" />
+          </div>
+          <h3>Tri Fold Brochure</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://www.behance.net/jdesignsolutions" className="btn">
+              Behance
+            </a>
+            <a href={Trifold} download className="btn btn-primary">
+              Download jpeg
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Porfolio;
--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -1,8 +1,82 @@
 import React from 'react';
 import './Testimonial.css';
+import avtr1 from '../../assets/avtr11.jpg';
+import avtr2 from '../../assets/avtr22.jpg';
+import avtr3 from '../../assets/avtr33.jpg';
+import avtr4 from '../../assets/avtr44.jpg';
+import avtr5 from '../../assets/avtr55.jpg';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    Avatar: avtr1,
+    Name: 'Dindo Leonard Paran',
+    Review:
+      'Throughout the graphic design process, we had a terrific time working with Jayson. He communicates effectively and simply. Jaysons brilliance is demonstrated by his ability to take our vision/mission and turn it into a logo that accurately reflects it.',
+  },
+  {
+    Avatar: avtr2,
+    Name: 'Jajie Jam Abergas',
+    Review:
+      'Jayson has an endless supply of innovative ideas, which he uses to consistently surprise and excite me with every creative design task I give him. Jayson is constantly coming up with new ideas. And his ideas arent merely for show. They are always making a point or selling an idea.',
+  },
+  {
+    Avatar: avtr3,
+    Name: 'Algyn Piezas',
+    Review:
+      'Jayson and I collaborated on a number of projects, including a complete overhaul and re-design of our website. He is a gifted artist with a wide range of abilities. I continue to use his services to this day since he is always quick to react to requests and extremely thorough. He is unquestionably the greatest. Thank you so much, Jayson!',
+  },
+  {
+    Avatar: avtr4,
+    Name: 'Analyn Naca',
+    Review:
+      'Jayson is the welcoming face of the graphic design profession. Jayson has designed logos, marketing collateral, and websites for us, and he is quick, simple to work with, and provides excellent value on every assignment. We cant afford a full-time graphic designer, but Jayson can help us create a memorable brand.',
+  },
+  {
+    Avatar: avtr5,
+    Name: 'Gimmariese Diaz',
+    Review:
+      'Jayson did an outstanding job developing a logo, website, and other advertising materials for me, and he did it swiftly! Every time I have a need, I will turn to Jayson for assistance. I cant say enough good things about the experience, Jayson.',
+  },
+];
 
 const Testimonial = () => {
-  return <section id="testimonial">Testimonial</section>;
+  return (
+    <section id="Testimonials">
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        className="container testimonials__container"
+        // install swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ Avatar, Name, Review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={Avatar} alt="Avatar4" />
+              </div>
+              <h5 className="client__name">{Name} </h5>
+              <small className="client__review">{Review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonial;
